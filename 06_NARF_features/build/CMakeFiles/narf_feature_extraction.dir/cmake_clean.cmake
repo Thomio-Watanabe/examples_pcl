620fc3bdbcc1ad00304a62cfb6b6068e38c8707c
Thomio Watanabe <thomio.watanabe@gmail.com>
2014-07-14 09:51:39 -0300

    NARF feature tutorial added

---
+++ 06_NARF_features/build/CMakeFiles/narf_feature_extraction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/narf_feature_extraction.dir/narf_feature_extraction.cpp.o"
+  "narf_feature_extraction.pdb"
+  "narf_feature_extraction"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/narf_feature_extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
